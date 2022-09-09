f7f9c6925b34ee7088e1c4280277ae31dd9406f4
osho210 <anysense.yamatsu@gmail.com>
2022-09-09 16:24:16 +0900

    react雛壇の作成

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+const Main =(<h1>Markdown Editor</h1>)
+
+//画面を描写する
+render(Main,document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  module: {
+    //ビルド時に追加で行う処理
+    rules: [
+      {
+        //拡張子がtsの場合ts-loaderを実行する
+        test: /\.tsx$/,
+        use: "ts-loader",
+        //除外するファイル
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  //解決するファイルの拡張子
+  resolve: {
+    //tsx = ts + jsx を使用できる
+    extensions: [".js", ".ts", "tsx"],
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    publicPath: "dist/",
+  },
+};
