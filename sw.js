e9030b1c31970c80c6e2ad0c410c64a73770c23a
osho210 <anysense.yamatsu@gmail.com>
2022-09-14 14:30:18 +0900

    local確認

--- sw.js
+++ sw.js
@@ -1,8 +1,28 @@
+const CacheName = "Cache:v1"; //キャッシュの名前
+
 //サービスワーカーを指す
 self.addEventListener("install", (event) => {
   console.log("ServiceWorker install", event);
 });
 
+const networkFallingBackToCache = async (request) => {
+  const cache = await caches.open(CacheName);
+  try {
+    const response = await fetch(request);
+    await cache.put(request, response.clone());
+    return response;
+  } catch (err) {
+    console.error(err);
+    return cache.match(request);
+  }
+};
+
 self.addEventListener("active", (event) => {
   console.log("ServiceWorker active", event);
+});
+
+//イベント時に実行する処理
+// ネットワークを経由してリソースを取得するためのAPI
+self.addEventListener("fetch", (event) => {
+  event.respondWith(networkFallingBackToCache(event.request));
 });
